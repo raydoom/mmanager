48a7c7f9c0736bf48451e478b56046b1f7a461ff
maxd <raydoom@163.com>
2018-11-23 21:55:06 +0800

    rename mmanager_app to app

--- mmanager/urls.py
+++ mmanager/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mmanager_app import views
+from app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls,name=admin),
